aa550b219f00f3d714c9ea640b4de8e341c5c685
Yunus Yesil <yunusyesil333@gmail.com>
2024-10-07 18:14:45 +0200

    created a method called doubled_even_digits and worked on the method of sum_even_two_digits

--- credit_card_validator.py
+++ credit_card_validator.py
@@ -2,6 +2,10 @@ class credit_card_validator:
     
     def __init__(self):
         self.list_modified_num = None
+        self.even_doubled_nums = []
+        self.even_doubled_nums_two_digits = []
+        self.index_even_doubled_nums = 0
+        self.sum_even_doubled_nums_two_digits = []
 
     def validation(self):
         self.num = input("Enter a credit card #: ")
@@ -22,34 +26,35 @@ class credit_card_validator:
         total_even = 0
         index_even = 0
         sum_even_digits = 0
-        even_doubled_nums = []
-        even_doubled_nums_two_digits = []
         sum_digits = []
-        index_even_doubled_nums = 0
         for i in self.list_modified_num:
             if index_even % 2 == 0:
                 total_even = int(i)*2
-                even_doubled_nums.append(total_even)
+                self.even_doubled_nums.append(total_even)
             index_even += 1
-        print(even_doubled_nums)
-        ##
-        for i in even_doubled_nums:
+        print(self.even_doubled_nums)
+        
+    def sum_even_two_digits(self):
+        for i in self.even_doubled_nums:
             if i > 9:
-                even_doubled_nums_two_digits.append(i)
-                index_even_doubled_nums += 1
+                self.even_doubled_nums_two_digits.append(i)
+                self.index_even_doubled_nums += 1
             else:
-                index_even_doubled_nums += 1
-        print(even_doubled_nums_two_digits)
-        
-    def sum_even_two_digits():
-        pass
-        
+                self.index_even_doubled_nums += 1
+        print(self.even_doubled_nums_two_digits)      
+        for number in self.even_doubled_nums_two_digits:
+            digit_sum = 0
+            for digit in str(number):
+                digit_sum += int(digit)
+            self.sum_even_doubled_nums_two_digits.append(digit_sum)
+        print(self.sum_even_doubled_nums_two_digits)
                   
 def main():
     cr = credit_card_validator()
     cr.validation()
     cr.sum_odd_digits()
     cr.double_even_digits()
+    cr.sum_even_two_digits()
 
 if __name__ == "__main__":
     main()
